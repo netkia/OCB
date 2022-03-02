bc984967c4f29baa8fb84445b9ec283c131b1fc9
Jesús Cuevas <jesus.cuevas@netkia.es>
2022-03-02 12:42:20 +0100

    Agregado contexto para entrega parcial

--- addons/stock/models/stock_move.py
+++ addons/stock/models/stock_move.py
@@ -1543,10 +1543,11 @@ class StockMove(models.Model):
                 qty_split = move.product_uom._compute_quantity(move.product_uom_qty - move.quantity_done, move.product_id.uom_id, rounding_method='HALF-UP')
                 new_move_vals = move._split(qty_split)
                 backorder_moves_vals += new_move_vals
-        backorder_moves = self.env['stock.move'].create(backorder_moves_vals)
-        backorder_moves._action_confirm(merge=False)
-        if cancel_backorder:
-            backorder_moves.with_context(moves_todo=moves_todo)._action_cancel()
+        if not self.env.context.get('not_create_move', False):
+            backorder_moves = self.env['stock.move'].create(backorder_moves_vals)
+            backorder_moves._action_confirm(merge=False)
+            if cancel_backorder:
+                backorder_moves.with_context(moves_todo=moves_todo)._action_cancel()
         moves_todo.mapped('move_line_ids').sorted()._action_done()
         # Check the consistency of the result packages; there should be an unique location across
         # the contained quants.
