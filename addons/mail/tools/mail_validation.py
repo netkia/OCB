c32d77c45823c29918cdb39fc8739b6ce7f1b7cb
hugomartineznetkia <hugo.martinez@netkia.es>
2024-02-28 14:12:50 +0100

    quitar validacion del flanker

--- addons/mail/tools/mail_validation.py
+++ addons/mail/tools/mail_validation.py
@@ -8,20 +8,20 @@ _logger = logging.getLogger(__name__)
 
 _flanker_lib_warning = False
 
-try:
-    from flanker.addresslib import address
-    # Avoid warning each time a mx server is not reachable by flanker
-    logging.getLogger("flanker.addresslib.validate").setLevel(logging.ERROR)
+# try:
+#     from flanker.addresslib import address
+#     # Avoid warning each time a mx server is not reachable by flanker
+#     logging.getLogger("flanker.addresslib.validate").setLevel(logging.ERROR)
 
-    def mail_validate(email):
-        return bool(address.validate_address(email))
+#     def mail_validate(email):
+#         return bool(address.validate_address(email))
 
-except ImportError:
+# except ImportError:
 
-    def mail_validate(email):
-        global _flanker_lib_warning
-        if not _flanker_lib_warning:
-            _flanker_lib_warning = True
-            _logger.info("The (optional) `flanker` Python module is not installed,"
-                         "so email validation will fallback to email_normalize.")
-        return tools.email_normalize(email)
+def mail_validate(email):
+    global _flanker_lib_warning
+    if not _flanker_lib_warning:
+        _flanker_lib_warning = True
+        _logger.info("The (optional) `flanker` Python module is not installed,"
+                     "so email validation will fallback to email_normalize.")
+    return tools.email_normalize(email)
